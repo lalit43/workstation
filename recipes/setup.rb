6705812c9479951e6e7ec9712360ff1322786017
lalit43 <lalit43@hotmail.com.com>
2017-09-03 04:23:50 +0000

    added user and group

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -12,6 +12,17 @@ end
 
 template '/etc/motd' do
   source 'motd.erb'
+  variables(
+     :name => 'technotrainer'
+)
   action :create
 end
-
+user 'user1' do
+comment 'user1'
+uid '123'
+home '/home/user1'
+shell '/bin/bash'
+end
+group 'admins' do
+  members 'user1'
+end
